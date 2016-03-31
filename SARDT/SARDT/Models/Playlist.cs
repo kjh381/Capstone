cd4e16224d6f1f31a2df52878c80b0d05b492ad7
andreweskild <andreweskild@gmail.com>
2016-03-30 23:46:24 -0700

    Created Playlist Class

---
+++ SARDT/SARDT/Models/Playlist.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Google.YouTube;
+
+namespace SARDT.Models
+{
+    public class Playlist
+    {
+        public Playlist()
+        {
+
+        }
+
+        public List<Video> VideoList { get; set; }
+    }
+}
