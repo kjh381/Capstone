5c7de4eef5c6f2450784f3b726d5fe6419a3d638
kjh381 <kjh12@aol.com>
2016-04-02 10:28:48 -0700

    More work on initializing DB
    Started adding required parts for editing text and discovered the DB is
    not creating. Working on connection string and initializer still.

---
+++ SARDT/SARDT/Controllers/WebContentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SARDT.Controllers
+{
+    public class WebContentController : Controller
+    {
+        //
+        // GET: /WebContent/
+        public ActionResult EditText()
+        {
+            return View();
+        }
+	}
+}
--- SARDT/SARDT/Models/DBInitializer.cs
+++ SARDT/SARDT/Models/DBInitializer.cs
@@ -3,20 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using SARDT.Models;
 
 namespace SARDT.Models
 {
     public class DBInitializer : DropCreateDatabaseAlways<SARDTContext>
     //public class DBInitializer : DropCreateDatabaseIfModelChanges<SARDTContext>
     {
-
         protected override void Seed(SARDTContext context)
         {
             //TODO: Add seeds to context here. 
-            //context.class.add(newObjName)
+            //context.class.Add(newObjName)
+            WebText homeText = new WebText{ WebTextID = 0, Section="home", Body = "Here is some test text for the home page", LastChangedOn=new DateTime(2016,4,2), LastChangeBy="Kyle"};
+            context.WebTexts.Add(homeText);
 
+            WebText aboutText = new WebText { WebTextID = 1, Section = "about", Body = "Info about the dive team", LastChangedOn = new DateTime(2016, 4, 2), LastChangeBy = "Kyle" };
+            context.WebTexts.Add(aboutText);
+
+            WebText historyText = new WebText { WebTextID = 2, Section = "History", Body = "History of the diveteam", LastChangedOn = new DateTime(2016, 4, 2), LastChangeBy = "Kyle" };
+            context.WebTexts.Add(historyText);
 
             base.Seed(context);
+            context.SaveChanges();
         }
     }
 }
