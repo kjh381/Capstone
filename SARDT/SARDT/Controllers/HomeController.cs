83b1866d9008f67516d1dbba4555fd7affba1e79
drgizmo666 <james_e_powell@live.com>
2016-04-20 15:35:48 -0700

    add public events to homepage
    got it to add events type public to home still need to validate the data

--- SARDT/SARDT/Controllers/HomeController.cs
+++ SARDT/SARDT/Controllers/HomeController.cs
@@ -19,6 +19,33 @@ namespace SARDT.Controllers
                                select s).FirstOrDefault();
 
             ViewBag.BodyText = home.Body;
+
+            List<String> publicEvents = new List<String>();
+
+            //foreach (Event e in db.Events)
+            //{
+            //    var events = (from ev in db.Events
+            //                    where ev.Type == "public"
+            //                        select ev.EventTitle);
+
+            //    string showEvent = events.ToString();
+
+            //    publicEvents.Add(showEvent);
+            //}
+
+            var events = from ev in db.Events
+                         where ev.Type == "public"
+                         select ev;
+
+            foreach (var e in events)
+            {
+                var eventDate = e.EventDate.Date;
+                var eventTime = ParseMilitaryTime(e.StartTime.ToString());
+
+                publicEvents.Add(eventDate.ToString("d") + " " + e.EventTitle + " " + eventTime.ToString("t"));
+            }
+        
+            ViewBag.EventsText = publicEvents;
             
             Video currentVideo = new Video();
             currentVideo.Title = "invalid";
@@ -64,5 +91,32 @@ namespace SARDT.Controllers
         {
             return View();
         }
+        public static DateTime ParseMilitaryTime(string time)
+        {
+	    //
+	    // Convert hour part of string to integer.
+	    //
+	    string hour = time.Substring(0, 2);
+	    int hourInt = int.Parse(hour);
+	    if (hourInt >= 24)
+	    {
+	        throw new ArgumentOutOfRangeException("Invalid hour");
+	    }
+	    //
+	    // Convert minute part of string to integer.
+	    //
+	    string minute = time.Substring(2, 2);
+	    int minuteInt = int.Parse(minute);
+	    if (minuteInt >= 60)
+	    {
+	        throw new ArgumentOutOfRangeException("Invalid minute");
+	    }
+	    //
+	    // Return the DateTime.
+	    //
+	    return new DateTime(2016, 04, 20, hourInt, minuteInt, 0);
+        }
+
+
     }
 }
